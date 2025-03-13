07cff478304a0ec7d0d3893e53afa4a24867d7a3
Sarah <sarah.mesch1@iu-study.org>
2025-03-13 20:26:59 +0100

    script.js .getElementById ersetzen

--- script.js
+++ script.js
@@ -18,17 +18,9 @@ document.querySelectorAll('.home').forEach(item => {
     });
 });
 
-// Menü schließen, wenn auf "Über mich" geklickt wird
-document.getElementById('.ueber_mich').addEventListener('click', function() {
-    document.getElementById('menu').style.display = 'none';
-});
-
-// Menü schließen, wenn auf "Mein Weg als Sportlerin" geklickt wird
-document.getElementById('.sportlerin').addEventListener('click', function() {
-    document.getElementById('menu').style.display = 'none';
-});
-
-// Menü schließen, wenn auf "Fanclub Meschi#11" geklickt wird
-document.getElementById('.fanclub').addEventListener('click', function() {
-    document.getElementById('menu').style.display = 'none';
+// Menü schließen, wenn auf "Über mich", "Mein Weg als Sportlerin" oder "Fanclub" geklickt wird
+document.querySelectorAll('.ueber_mich, .sportlerin, .fanclub').forEach(item => {
+    item.addEventListener('click', function() {
+        document.getElementById('menu').style.display = 'none';
+    });
 });
